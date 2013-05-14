2cd95754d5ed88453cbbdb08a2b17861c385aa77
wyleung <w.y.leung@e-sensei.nl>
2013-05-14 17:12:37 +0200

    Add NFS mounting options on first boot

---
+++ conf/firstboot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dialog --title "Configuring NFS virdir share" --infobox "Waiting for NFS mount..." 5 50
+
+NETWORK_SEGMENT=`ifconfig eth0 | egrep 'inet addr' | cut -f3 -d'.'`
+VIRDIR_GROUP=MY_SARA_VIRDIR
+FSTAB_LINE="10.0.${NETWORK_SEGMENT}.4:/${VIRDIR_GROUP}	/virdir	nfs4	noauto,soft,bg	0	0"
+
+echo $FSTAB_LINE >> /etc/fstab
+
+# change the rc.local to default
+sed -i 's_sh /root/firstboot.sh_exit 0_' /etc/rc.local
+
+# return exit code 0 (all of above actions where successfull)
+exit 0
