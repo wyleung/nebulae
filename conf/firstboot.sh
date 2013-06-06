9c29a7635099d8cfd7106227912537f6e0f66ad3
wyleung <w.y.leung@e-sensei.nl>
2013-06-06 12:55:05 +0200

    Fix: create virdir directory before mounting the nfs

--- conf/firstboot.sh
+++ conf/firstboot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 dialog --title "Configuring NFS virdir share" --infobox "Waiting for NFS mount..." 5 50
 
+mkdir -p /virdir
+chmod 777 /virdir
+
 NETWORK_SEGMENT=`ifconfig eth0 | egrep 'inet addr' | cut -f3 -d'.'`
 VIRDIR_GROUP=MY_SARA_VIRDIR
 FSTAB_LINE="10.0.${NETWORK_SEGMENT}.4:/${VIRDIR_GROUP}	/virdir	nfs4	noauto,soft,bg	0	0"
